2391c49687d2e182a1d7c374035438a687fde0bd
mskramstad <68239718+mskramstad@users.noreply.github.com>
2025-02-11 10:54:11 -0800

    Update DiceSimulationGUI.java

--- DiceSimulationGUI.java
+++ DiceSimulationGUI.java
@@ -18,9 +18,7 @@ public class DiceSimulationGUI extends JPanel
 
     public void paintComponent(Graphics g)  // draw graphics in the panel
     {
-        int width = getWidth();             // width of window in pixels
-        int height = getHeight();           // height of window in pixels
-
+      
         super.paintComponent(g);            // call superclass to make panel display correctly
         g.setColor(Color.WHITE);
         //output a message at top of screen (x = 50, y = 50)
@@ -28,43 +26,14 @@ public class DiceSimulationGUI extends JPanel
 
         //initialize the following variables, r1=0, r2=1, counter, startX = 70, startY = 70
         int r1 = 0;
-        int r2 = 2;
-        int startX = 70;
-        int startY = 70;
-        int counter = 0;
-
-        //create a while loop that checks that r1 and r2 are not equal
-        while (r1 != r2) {
-        // get two random numbers and assign it to r1 and r2
+       
         r1 = getRandomNumber();
-        r2 = getRandomNumber();
         
-
-        // do this last -- if counter = 10 assign startX=300, startY = 70; moves to new column and back to top.  Do the same for 20 and 30.
-        if (counter == 7) {
-            startX = 300;
-            startY = 70;
-        }
-        if (counter == 14) {
-            startX = 500;
-            startY = 70;
-        }
-
-        if (counter == 21) {
-            startX = 700;
-            startY = 70;
-        }
-
         // call showDie method sending Graphics object, the die filename "die" + r1 + ".jpg", the value (1-6), x, y
         showDie(g, "dice/die" + r1 + ".jpg", startX, startY);
-        showDie(g, "dice/die" + r2 + ".jpg",startX+75, startY);
         //increment the startY value by 75 (moves down 75 if it's not equal)
-        startY+=75;
-        //increment counter by 1
-        counter ++;
-        }
-        // draw a message on screen that outputs "Total times: " + counter.
-        g.drawString("Total times: " + counter, startX, startY + 50);
+       
+        g.drawString("You rolled a : " + (r1+1));
         
     }
 
